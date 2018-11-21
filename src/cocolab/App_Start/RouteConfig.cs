652bf38a4844ecabff40bd702bd7706fa3d13314
Tritiummonoid <tritiummonoid@gmail.com>
2018-11-20 21:56:10 -0800

    agregar ASP.NET hello world

--- src/cocolab/App_Start/RouteConfig.cs
+++ src/cocolab/App_Start/RouteConfig.cs
@@ -10,6 +10,12 @@ namespace cocolab
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "HelloWorld",
+                url: "hello-world",
+                defaults: new { controller = "HelloWorld", action = "Index" }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
