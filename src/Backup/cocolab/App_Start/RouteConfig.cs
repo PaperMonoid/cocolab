49663a7dc8606ad98ec278d951742491cdc48d60
pablo-aguiar <pablo.aguiar17@tectijuana.edu.mx>
2018-11-21 17:56:14 -0800

    framework, nhibernate y model Alumno

---
+++ src/Backup/cocolab/App_Start/RouteConfig.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace cocolab
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "HelloWorld",
+                url: "hello-world",
+                defaults: new { controller = "HelloWorld", action = "Index" }
+            );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ src/cocolab/Models/Alumno.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NHibernate;
+
+namespace cocolab.Models
+{
+    public class Alumno
+    {
+        public int id_alumno { get; set; }
+        public string nombre { get; set; }
+        public string apellido_paterno { get; set; }
+        public string apellido_materno { get; set; }
+        public string id_carrera { get; set; }
+        public bool estatus_alumno { get; set; }
+        public DateTime fecha_registro_alumno {get; set;}
+        public DateTime fecha_modificacion_alumno { get; set; } 
+
+        
+    }
+}
